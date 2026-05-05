08a0e4c24e43563929e078ef4d15b8bae822571c
Saksham Jain <saksham.jain@sap.com>
2026-05-05 11:34:02 +0530

    feat: Product Filters

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { listProducts, getProduct, getVariations, getCategories } from '../controllers/product.controller.js';
+import { listProducts, getProduct, getVariations, getCategories, getAttributes } from '../controllers/product.controller.js';
 
 const router = express.Router();
 
 router.get('/', listProducts);
 router.get('/categories', getCategories);
+router.get('/attributes', getAttributes);
 router.get('/:id', getProduct);
 router.get('/:id/variations', getVariations);
 
